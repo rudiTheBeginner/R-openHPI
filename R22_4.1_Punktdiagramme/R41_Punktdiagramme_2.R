0c7e9ab6984945156fb6dc525d145f2f82fc0a1d
rudiTheBeginner <103328549+rudiTheBeginner@users.noreply.github.com>
2022-04-10 11:04:11 +0200

    Upload 4.1 Punktdiagramme

---
+++ R22_4.1_Punktdiagramme/R41_Punktdiagramme_2.R
@@ -0,0 +1,13 @@
+codeoceanR::rt_score()
+codeoceanR::rt_plot1() # Außerhalb von CodeOcean tut dieser Befehl nichts
+
+# B1 ----
+# Kopiere den Code des letzten Skriptes.
+# Verwende blaue (0000CD5D) Punkte für Provinzen,
+# in denen die Mehrheit der Bevölkerung (>50%) katholisch ist.
+# Es gibt mehrere Lösungen, die im Forum diskutiert werden können :)
+# tb1_start
+
+# tb1_ende
+
+codeoceanR::rt_plot2()
