ffe762ebf4312dc685b2cec1cc2b649f67c619fa
Rudolf Riegel <rudolf_riegel@hotmail.com>
2022-04-10 19:21:14 +0200

    Solve task 2

--- R22_4.1_Punktdiagramme/R41_Punktdiagramme_2.R
+++ R22_4.1_Punktdiagramme/R41_Punktdiagramme_2.R
@@ -7,7 +7,12 @@ codeoceanR::rt_plot1() # Außerhalb von CodeOcean tut dieser Befehl nichts
 # in denen die Mehrheit der Bevölkerung (>50%) katholisch ist.
 # Es gibt mehrere Lösungen, die im Forum diskutiert werden können :)
 # tb1_start
-
+plot(swiss$Education, swiss$Examination,
+     xlab = "Hochschulbildung  [%]", 
+     ylab = "höchste Note in der Armeeprüfung  [%]",
+     main = "Schweiz, 1888", las = 1,
+     pch = 16, cex = (swiss$Fertility/15), col = ifelse(swiss$Catholic > 50,"#0000CD5D", "#00CD005D")
+)
 # tb1_ende
 
 codeoceanR::rt_plot2()
